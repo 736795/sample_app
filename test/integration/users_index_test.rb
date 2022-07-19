e0474ce97a82b117bf7470c07c86a6d719ef8d97
shintani.nobuyuki <mnobuyuki02@gmail.com>
2022-07-19 14:05:21 +0000

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,7 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin     = users(:michael)
     @non_admin = users(:archer)
-  end
+  ends
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
@@ -29,4 +29,5 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
+end
 end
